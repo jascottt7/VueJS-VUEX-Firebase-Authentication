533466bc47dfe4f669533f3c784fc827f92dddac
jascottt7 <jayscott1@gmail.com>
2017-08-10 15:41:03 +0000

    test

--- src/firebaseApp.js
+++ src/firebaseApp.js
@@ -1,7 +1,12 @@
 import firebase from 'firebase';
 
 const config = {
-  //add firebase config info here.
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
 };
 
 export const firebaseApp = firebase.initializeApp(config);
